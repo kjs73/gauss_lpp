167978bae7944562d04fbeaed338418c7c47fb15
Julian Schrenk <kjs73@cam.ac.uk>
2014-11-16 21:37:25 +0000

    formatting

--- schramm_equation.hpp
+++ schramm_equation.hpp
@@ -9,11 +9,11 @@ class schramm_equation{
 public:
     typedef long double float_t;
 private:
-	const float_t kappa;
-	const float_t EPS;
-	const bool space_filling;
-	const float_t constant;
-	gauss_transformations gh;
+    const float_t kappa;
+    const float_t EPS;
+    const bool space_filling;
+    const float_t constant;
+    gauss_transformations gh;
 public:
     schramm_equation(const float_t kappa_)
         : kappa(kappa_),
@@ -22,28 +22,28 @@ public:
           constant(space_filling ? 0 : tgamma(4 / kappa_) / std::sqrt(M_PI) / tgamma((8 - kappa_) / (2 * kappa_))),
           gh(gauss_transformations(float_t(1) / float_t(2), 4 / kappa_, float_t(3) / float_t(2)))
     {}
-	float_t operator()(const float_t phi) const
+    float_t operator()(const float_t phi) const
     {
-	    if (phi < 0 || phi > M_PI) {
-	        throw std::runtime_error("schramm_equation::operator(): illegal input");
-	    }
+        if (phi < 0 || phi > M_PI) {
+            throw std::runtime_error("schramm_equation::operator(): illegal input");
+        }
         if (phi < EPS) {
-	        return 1;
-	    }
+            return 1;
+        }
         if (space_filling) {
-	        return float_t(0.5);
-	    }
-		const float_t tmp = cot(phi);
+            return float_t(0.5);
+        }
+        const float_t tmp = cot(phi);
         const float_t result = float_t(1) / float_t(2) + constant * tmp * gh(-tmp * tmp);
         if (result < -EPS || result > 1 + EPS) {
             throw std::runtime_error("schramm_equation::operator() evaluation failed");
         }
-		return std::min<float_t>(std::max<float_t>(result, 0), 1);
-	}
-	float_t cot(const float_t phi) const
+        return std::min<float_t>(std::max<float_t>(result, 0), 1);
+    }
+    float_t cot(const float_t phi) const
     {
         return float_t(1) / std::tan(phi);
-	}
+    }
 };
 
 #endif//SCHRAMM_EQUATION_HPP
